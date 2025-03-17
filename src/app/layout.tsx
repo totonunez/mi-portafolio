a3b1c05dbd83d330b03256229f0e975fa6fa67ff
cristobalnunezqcart <cristobalnunez@comoquiero.cl>
2025-03-17 14:44:05 -0300

    agregar Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,14 @@ import Script from 'next/script';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      
       <Head>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         />  
       </Head>
+      
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-SXNWCZEJVT"
@@ -34,7 +36,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `,
         }}
       />
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5NTC9M5F');
+          `,
+        }}
+      />
       <body className="min-h-screen flex flex-col">
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5NTC9M5F"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
         <Navbar />
         <main className="flex-1">
           
